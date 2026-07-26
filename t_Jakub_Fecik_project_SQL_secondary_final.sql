c13ccb9bb530b35407ec655681e005268cf2f528
Jakub8b <jakubfecik111@gmail.com>
2026-07-26 20:34:10 +0200

    Update t_Jakub_Fecik_project_SQL_secondary_final.sql

--- t_Jakub_Fecik_project_SQL_secondary_final.sql
+++ t_Jakub_Fecik_project_SQL_secondary_final.sql
@@ -1,7 +1,6 @@
 -- Jako dodatečný materiál připravte i tabulku s HDP, GINI koeficientem a populací dalších evropských států ve stejném období, jako primární přehled pro ČR.
 
 create table Additional_table_EUGDP as 
-
 select
 c.country,
 e.year,
@@ -13,5 +12,5 @@ from economies e
 join countries c 
 on e.country = c.country
 where continent = 'Europe'
-and year >= 1999
+and year >= '2006'and year <= '2018'
 order by e."year", c.country;
